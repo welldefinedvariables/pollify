424e642bd727199bf4089fa6f1e0cd0e89e87b4d
louisalbano <louisalbano21@gmail.com>
2014-11-18 18:33:18 -0500

    blank project for poll app

---
+++ Pollify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pollify
+//
+//  Created by Louis Albano on 11/18/14.
+//  Copyright (c) 2014 Louis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
